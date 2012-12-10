d69532ef2ab2da0b73e47a9b767891465f61dcc1
Devin Schwab <digidevin@gmail.com>
2012-12-10 02:01:26 -0500

    Added the basic polynomial basis function from the example

--- src/lspi_examples/chain.py
+++ src/lspi_examples/chain.py
@@ -103,3 +103,35 @@ class Simulator(BaseSim):
         """
 
         return [i for i in range(self.A)]
+
+def basis_pol(state=None, action=None):
+    """
+    Computes a set of polynomial (on "state") basis functions
+    up to a certain degree. The set is duplicated for each action.
+    The action determines which segment will be active
+    """
+
+    degpol = 4; # degree of the polynomial
+
+    numbasis = (degpol+1) * A
+
+    if state is None or action is None:
+        return numbasis
+
+    # initialize
+    phi = np.zeros((numbasis, 1))
+
+    # check if stat is within bounds
+    if state < 0 or state >= S:
+        raise IndexError('%i is out of bounds' % state)
+
+    # find the starting position
+    base = action * (numbasis/A)
+
+    # compute the polynomial terms
+    phi[base] = 1
+
+    for i in range(1, degpol+1):
+        phi[base+i] = phi[base+i-1] * (10.0*(state+1)/S)
+
+    return phi
