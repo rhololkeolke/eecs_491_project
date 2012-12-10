31eb44811f129bb3a2993b00579e994c31ce50ae
Devin Schwab <digidevin@gmail.com>
2012-12-09 23:14:21 -0500

    Added an abstract base class for simulator classes

---
+++ src/lspi_framework/simulator.py
@@ -0,0 +1,43 @@
+"""
+This module provides the basic abstract base class for a simulator
+"""
+
+import abc
+
+class Simulator(object):
+    """
+    ABC of all simulators
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def reset(self, state=None):
+        """
+        Resets the simulator to the specificed state.
+        If the specified state is None then a random
+        state is chosen
+        """
+        return
+
+    @abc.abstractmethod
+    def execute(self, action):
+        """
+        Executes the specified action
+
+        Returns a sample object
+        """
+
+    @abc.abstractmethod
+    def get_actions(self):
+        """
+        Returns a list of possible actions
+        """
+        return
+
+    @abc.abstractmethod
+    def get_state(self):
+        """
+        Returns the current state of the simulator
+        """
+        return
